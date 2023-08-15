9b5a0cb6a47034b03ca0b174fcc665450c95d1be
LeonardoStrechar <strechar21@gmail.com>
2023-08-15 10:08:38 -0300

    Refactoring: remove files unnecessary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      },  
     },
+    colors: {
+      BlackRussian: '08080A',
+      Amaranth: 'F5204F'
+    }
   },
   plugins: [],
 }
