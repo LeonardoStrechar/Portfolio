08f290688228bfa904313394f8ac3ae68cd94d0b
LeonardoStrechar <strechar21@gmail.com>
2023-09-22 09:04:29 -0300

    Feat: New about session

---
+++ src/app/components/teste/teste.jsx
@@ -0,0 +1,12 @@
+
+export function Teste (){
+
+
+    return (
+        <div className="border-5 border-blue-600 z-50">
+            <canvas id="canva"></canvas>
+            <script src="../../script/StarFieldCanvas.js"></script>
+            <script src="../../script/demo.js"></script>
+        </div>
+    )
+}
