ec610751cefab76290c9ae39734c29d14cd2e6c0
LeonardoStrechar <strechar21@gmail.com>
2023-10-11 12:00:04 -0300

    Feat: code refatoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import { useEffect } from "react";
 import { About } from "./components/about/about";
 import { Header } from "./components/header/header";
 import { Home } from "./components/home/home";
 import { SideInfo } from "./components/sideInfo/sideInfo";
 import { Technologies } from "./components/technologies/technologies";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import { Certificate } from "./components/certificates/certificates";
 
 export default function Content(){
-  // useEffect(() => {
-  //   AOS.init();
-  // }, []);
   return (
     <div data-aos="fade-up" className="flex flex-col justify-center">
       <Header/>
@@ -18,6 +13,7 @@ export default function Content(){
       <SideInfo/>
       <About/>
       <Technologies />
+      <Certificate />
     </div>
   )
 }
