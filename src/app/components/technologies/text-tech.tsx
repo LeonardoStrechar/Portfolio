e8048b52bbb99a783bf64757bc9394ccb9875ba3
LeonardoStrechar <strechar21@gmail.com>
2023-11-09 11:46:12 -0300

    feat/added text of technologies

--- src/app/components/technologies/text-tech.tsx
+++ src/app/components/technologies/text-tech.tsx
@@ -1,82 +1,82 @@
 export const textTech = [
     {
         "id":"icon-aws.svg",
-        "content": "teste de texto" 
+        "content": "AWS - The most famous cloud platform of world, I have more than 1 year of experience with the services: SQS, SNS, SES, Lambda Function, RDS, S3, ECS, ECR, EC2, ACM, WAF, Amplify, CloudFront, CloudWatch, LoadBalancer, API -Gateway and VPC. I am constantly looking to learn new services and I intend to seek new official AWS certificates." 
     },
     {
         "id":"icon-javascript.svg",
-        "content": "teste de texto" 
+        "content": "JavaScript - I have been working with Javascript for over 3 years, I discovered this language in 2019, I started learning how to manipulate DOM, create variables, functions, maps and develop more complex logic, I currently use JavaScript in its frameworks, such as ReactJS, NextJS and Express" 
     },
     {
         "id":"icon-html.svg",
-        "content": "teste de texto" 
+        "content": "HTML - my first experience in the technology area, I have been working with HTML for over 4 years, I have extensive experience with this markup language developing landing pages, portfolios, e-commerce platforms among other projects." 
     },
     {
         "id":"icon-css.svg",
-        "content": "teste de texto" 
+        "content": "CSS - The great terror of backend developers is my favorite, CSS, I have been working with this styling language for over 4 years, I have vast experience in creating responsive and beautiful websites, applying UI/UX concepts" 
     },
     {
         "id":"icon-typescript.svg",
-        "content": "teste de texto" 
+        "content": "TypeScript - I have 2 years of experience with this programming language, I use it together with frameworks such as Express and NextJS, using all possible types, such as: number, string, boolean, symbols, any, null, undefined, etc..." 
     },
     {
         "id":"icon-node.svg",
-        "content": "teste de texto" 
+        "content": "Node - I have more than 2 years of experience using this runtime to run my frontend and backend projects, I'm looking to improve my knowledge of this engine's architecture, testing how to increase performance by adding more threads..." 
     },
     {
         "id":"icon-express.svg",
-        "content": "teste de texto" 
+        "content": "Express - I have more than 2 years of experience developing backend systems, I can develop endpoints, integration with tools such as payment engines, database connection, AWS services, etc." 
     },
     {
         "id":"icon-graphql.svg",
-        "content": "teste de texto" 
+        "content": "GraphQL - I'm at the beginning of this tool to manage data transfer, I can develop queries, create mutations with e-commerce services, such as Shopify. I'm still in the learning phase." 
     },
     {
         "id":"icon-react.svg",
-        "content": "teste de texto" 
+        "content": "React - my main focus currently, I use it for frontend development, I have great experience in developing projects with react and using its hooks, such as UseState, UseRef, etc. I use this lib along with other technologies such as Tailwind CSS, NextJS, etc..." 
     },
     {
         "id":"icon-postgresql.svg",
-        "content": "teste de texto" 
+        "content": "PostgreSQL - Relational database, I worked a few times, but I know this architecture because it is close to MySQL, I have experience in diagrams such as MER and DER, creating queries and manipulating data easily" 
     },
     {
         "id":"icon-nextjs.svg",
-        "content": "teste de texto" 
+        "content": "NextJS - My favorite framework, I have more than 2 years of experience, I like to use it to develop my projects, with better SEO, I get great results, together with Tailwind CSS, I can develop entire pages in a few minutes!" 
     },
     {
         "id":"icon-tailwind.svg",
-        "content": "teste de texto" 
+        "content": "Tailwind CSS - CSS Framework, I have over 2 years of experience, great for accelerating the development of responsive websites and layouts, great tool to use with NextjS" 
     },
     {
         "id":"icon-bootstrap.svg",
-        "content": "teste de texto" 
+        "content": "BootStrap - great tool to help with frontend development, I used it a few times, I really like Boostrap Icons, which offers a wide range of images to use in the frontend" 
     },
     {
         "id":"icon-styled-components.svg",
-        "content": "teste de texto" 
+        "content": "Styled components - These tools are not used by me often, but they are a great option for frontend development, helping to build CSS" 
     },
     {
         "id":"icon-docker.svg",
-        "content": "teste de texto" 
+        "content": "Docker - System for managing containers, I have more than 2 years of experience developing Dockerfile and docker-compose files, I have already carried out integrations in deployment stages with docker-compose and bitbucket, saving ENV data when running the container, creating scripts to run projects , etc" 
     },
     {
         "id":"icon-mysql.svg",
-        "content": "teste de texto" 
+        "content": "MySQL - My first contact with a data base, I have more than 3 years of experience with this database, I have strong experience in creating small, medium and large databases, designing architecture with DER and MER, creating queries, relationships between tables , encryption of sensitive data" 
     },
     {
         "id":"icon-stripe.svg",
-        "content": "teste de texto" 
+        "content": "Stripe - Most famous payment engine in the world, I have 1 year of experience implementing systems to receive payments, using integrated checkout or split code to create custom forms" 
     },
     {
         "id":"icon-shopify.svg",
-        "content": "teste de texto" 
+        "content": "Shopify - famous e-commerce in the USA, used to develop online stores, I have 1 year of experience, I learned to use its own language (Liquid) based on React, I also use Shopify with NextJS and GraphQL" 
     },
     {
         "id":"icon-figma.svg",
-        "content": "teste de texto" 
+        "content": "Figma - Tool for developing layouts for websites and applications, I have 3 years of experience designing systems, I applied UI/UX rules, rules like 60-30-10, accessibility for new users. This Portfolio was thought and designed in Figma by me!" 
     },
     {
         "id":"icon-gcp.svg",
-        "content": "teste de texto" 
+        "content": "GCP - Google cloud tool, I used services such as ReCaptcha, GTM and Firebase, I have approximately 1 year of experience" 
     },
 ]
