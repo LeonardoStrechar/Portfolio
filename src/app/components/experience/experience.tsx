6ea5a3303eed04e4cfedba1bb471c955ee166dba
LeonardoStrechar <strechar21@gmail.com>
2023-11-21 16:30:12 -0300

    feat/ new experience component

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,13 @@
+export function Experience() {
+  return (
+    <div id="technologies" className="w-full flex flex-col items-center mb-5">
+      <div className="w-full text-center flex flex-col items-center">
+        <p className="text-amaranth text-xl mb-1">Experience</p>
+        <img src="linear-line.svg" alt="linear line" />
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
