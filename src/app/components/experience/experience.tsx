788c8a96d5f5a91a9c8185ee5dee23cb543b7732
LeonardoStrechar <strechar21@gmail.com>
2023-11-28 16:48:12 -0300

    feat/create box experience

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -5,8 +5,13 @@ export function Experience() {
         <p className="text-amaranth text-xl mb-1">Experience</p>
         <img src="linear-line.svg" alt="linear line" />
       </div>
-      <div>
-        
+      <div className="my-10">
+        <div className="border border-amaranth rounded-[33px] w-[350px] h-[420px] px-[1px] py-[1px] flex flex-col">
+          <img src="./conpanyImages/sideral.jpg" alt="conpany image" className="rounded-[33px] h-full w-full" />
+          <div className=" text-white h-full rounded-b-[33px] -mt-32 bg-blackRussian opacity-50 p-2 text-start">
+            <p>teste de texto</p>
+          </div>
+        </div>
       </div>
     </div>
   );
