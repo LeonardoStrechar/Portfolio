e171d90f21a5beae75b36b272320961878ea5382
LeonardoStrechar <strechar21@gmail.com>
2023-11-28 22:35:30 -0300

    feat/finish box expirence

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,17 +1,43 @@
 export function Experience() {
   return (
-    <div id="technologies" className="w-full flex flex-col items-center mb-5">
+    <div id="expirence" className="w-full flex flex-col items-center mb-20">
       <div className="w-full text-center flex flex-col items-center">
         <p className="text-amaranth text-xl mb-1">Experience</p>
         <img src="linear-line.svg" alt="linear line" />
       </div>
+      <div className="flex gap-5">
       <div className="my-10">
-        <div className="border border-amaranth rounded-[33px] w-[350px] h-[420px] px-[1px] py-[1px] flex flex-col">
+        <div className="border border-amaranth hover:border-white duration-200 rounded-[33px] w-[350px] h-[420px] px-[1px] py-[1px] flex flex-col">
           <img src="./conpanyImages/sideral.jpg" alt="conpany image" className="rounded-[33px] h-full w-full" />
-          <div className=" text-white h-full rounded-b-[33px] -mt-32 bg-blackRussian opacity-50 p-2 text-start">
-            <p>teste de texto</p>
+          <div className="text-white h-full rounded-b-[33px] -mt-32 hover:-mt-48 duration-200 bg-blackRussian bg-opacity-50 px-4 py-2 text-start">
+            <p className="opacity-100">Sideral linhas aereas</p>
+            <img src="./icon-download_cv_white.svg" width={25} height={25} className="my-1"/>
+            <p className="opacity-60 text-[12px]">Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves</p>
           </div>
         </div>
+      </div>
+
+      <div className="my-10">
+        <div className="border border-amaranth hover:border-white duration-200 rounded-[33px] w-[350px] h-[420px] px-[1px] py-[1px] flex flex-col">
+          <img src="./conpanyImages/sideral.jpg" alt="conpany image" className="rounded-[33px] h-full w-full" />
+          <div className="text-white h-full rounded-b-[33px] -mt-32 hover:-mt-48 duration-200 bg-blackRussian bg-opacity-50 px-4 py-2 text-start">
+            <p className="opacity-100">Sideral linhas aereas</p>
+            <img src="./icon-download_cv_white.svg" width={25} height={25} className="my-1"/>
+            <p className="opacity-60 text-[12px]">Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-10">
+        <div className="border border-amaranth hover:border-white duration-200 rounded-[33px] w-[350px] h-[420px] px-[1px] py-[1px] flex flex-col">
+          <img src="./conpanyImages/sideral.jpg" alt="conpany image" className="rounded-[33px] h-full w-full" />
+          <div className="text-white h-full rounded-b-[33px] -mt-32 hover:-mt-48 duration-200 bg-blackRussian bg-opacity-50 px-4 py-2 text-start">
+            <p className="opacity-100">Sideral linhas aereas</p>
+            <img src="./icon-download_cv_white.svg" width={25} height={25} className="my-1"/>
+            <p className="opacity-60 text-[12px]">Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves</p>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
   );
