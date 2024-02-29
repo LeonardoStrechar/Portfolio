4119db30fe197df974cb0b3220adfc406d5c4f94
LeonardoStrechar <strechar21@gmail.com>
2024-02-29 00:21:08 -0300

    feat/change content to expirence

--- src/components/experience/list-expirence.tsx
+++ src/components/experience/list-expirence.tsx
@@ -2,7 +2,7 @@ export const listExpirence = [
     {
         companyName: "MM Tecnologia de Informação LTDA",
         office: "Software Engineer",
-        description: "Atualmente trabalho como Engenheiro de Software, fornecendo novas soluções e correções de problemas em diversos sistemas de clientes, atuo principalmente no front-end utilizando ReactJS, NextJS, Tailwind CSS, AWS",
+        description: "Atualmente trabalho como Engenheiro de Software, fornecendo soluções e correções em diversos para clientes da empresa, atuo principalmente no front-end utilizando ReactJS, NextJS, Tailwind CSS, AWS, UI/UX",
         imageCompanyPath: "./conpanyImages/mm.jpg",
         imageIcon: "./icon-cpu.svg",
         workingTime: "2 Anos"
@@ -10,7 +10,7 @@ export const listExpirence = [
     {
         companyName: "A&M Solutions ",
         office: "Full-Stack Developer",
-        description: "Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves",
+        description: "Aqui foi meu primeiro contato com o mundo corporativo da tecnologia, nessa empresa, trabalhei como full-stack, desenvolvendo soluções com JavaScript e PHP, além de algumas libs para desenvolvimento como ReactJS",
         imageCompanyPath: "./conpanyImages/a&m.jpg",
         imageIcon: "./icon-cpu.svg",
         workingTime: "1 Ano"
@@ -18,7 +18,7 @@ export const listExpirence = [
     {
         companyName: "Freelancer",
         office: "Full-Stack Developer",
-        description: "Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves",
+        description: "Desde que entrei na vida de programador, trabalho como freelancer em minhas horas vagas do dia, criando novos sistemas ou corrigindo bugs em sistemas de clientes, com JavaScript, React, React-Native, PHP, AWS e outras tecnologias",
         imageCompanyPath: "./conpanyImages/freelance.jpg",
         imageIcon: "./icon-cpu.svg",
         workingTime: "4 Anos"
@@ -26,7 +26,7 @@ export const listExpirence = [
     {
         companyName: "Latal Embalagens Metalicas Ltda",
         office: "Aux. Laboratório",
-        description: "Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves",
+        description: "Meu primeiro contato CLT, 2 anos de puro aprendizado e esforço físico, iniciei trabalhando em linha de produção e nos últimos 8 meses, me tornei responsável pelo laboratório da empresa",
         imageCompanyPath: "./conpanyImages/latal.jpg",
         imageIcon: "./icon-book.svg",
         workingTime: "2 Anos"
@@ -34,7 +34,7 @@ export const listExpirence = [
     {
         companyName: "Sideral Linhas Aéreas Ltda",
         office: "Est. Administrativo",
-        description: "Nessa empresa atuei em varios setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves",
+        description: "Nessa empresa atuei em vários setores, desde o almoxarifado, administrativo e ajudante de mecânico de aero-naves",
         imageCompanyPath: "./conpanyImages/sideral.jpg",
         imageIcon: "./icon-book.svg",
         workingTime: "1 Ano"
