67f17e666eb6f271bb906d7a00a524644aa48e88
LeonardoStrechar <leonardostrechar@outlook.com>
2024-10-01 10:50:35 -0300

    feat: test redirect link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { About } from "../components/about/about";
 import { Header } from "../components/header/header";
 import { Home } from "../components/home/home";
@@ -8,8 +10,18 @@ import Projects from "../components/projects/project";
 import Contact from "../components/contact/contact";
 import Footer from "../components/footer/footer";
 import { Experience } from "../components/experience/experience";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Content(){
+
+  const router = useRouter()
+
+  useEffect(() => {
+    console.log("redirect")
+    router.push("https://sidcontrol.com.br/static/uploads/documento/Documento_75498_724_20240726101624.pdf")
+  }, [])
+  
   return (
     <div data-aos="fade-up" className="flex flex-col">
       <Header/>
